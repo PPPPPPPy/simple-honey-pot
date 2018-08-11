f5022576c3c4dc24f3241669a89f7bef22cfe5bc
Py <41265850+PPPPPPPy@users.noreply.github.com>
2018-08-11 13:22:56 +0800

    Update alarm.py

--- Alarm/alarm.py
+++ Alarm/alarm.py
@@ -6,3 +6,18 @@ while True:
         print "=== Attention -- Alarm Launched ==="
         libeep.beepAction (3,0.05,1)
         time.sleep(1)
+   
+
+''' # Latest Version
+# alarm.py
+import RPi.GPIO as GPIO
+import time
+import libeep
+
+libeep.beepAction (2,0.05,1)
+time.sleep(1)
+while True:
+        print "=== Attention -- Alarm Launched ==="
+        libeep.beepAction (1,0.05,1)
+        time.sleep(1)
+'''
